fc60ea3e5001e4a694f99124d54388b128635f20
Muhammad Kashif <125742946+kashifjavaid1@users.noreply.github.com>
2023-10-20 07:54:04 -0700

    commite

--- src/components/buttonOne/ButtonOne.js
+++ src/components/buttonOne/ButtonOne.js
@@ -5,7 +5,7 @@ import '../buttonOne/ButtonOne.css'
 export default function ButtonOne() {
   return (
     <div>
-        <button  className='btn2'><img src={img}  /><span className='title'>Get Started</span></button>
+        <button  className='btn2'><img src={img} /><span className='title'>Get Started</span></button>
     </div>
   )
 }
