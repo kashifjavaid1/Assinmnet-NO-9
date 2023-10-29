e8a08a3f1659de58cf589fec2389e7ef57ca399e
Muhammad Kashif <125742946+kashifjavaid1@users.noreply.github.com>
2023-10-29 05:17:29 -0700

    commite

--- src/components/subscribe/subscribeBtn/SubscribeBtn.js
+++ src/components/subscribe/subscribeBtn/SubscribeBtn.js
@@ -6,6 +6,7 @@ export default function SubscribeBtn() {
             <div class="input-group mb-3 ">
                 <input type="text" class="form-control border-0 rounded-5" placeholder="Enter your email here" aria-label="Recipient's username" aria-describedby="button-addon2" />
                 <button class="btn btn-sm button text-white rounded-4  fw-bold " type="button" >Subscribe</button>
+                <button>sumit</button>
             </div>
         </div>
     )
